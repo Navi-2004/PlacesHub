3f9b7f75e665bec1274af928f63a15d783a5a611
Navi <navisenthilnadhan@gmail.com>
2023-12-17 15:10:39 +0530

    baseURL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ import PlacePage from "./Pages/PlacePage";
 import BookingsPage from "./Pages/BookingsPage";
 import BookingPage from "./Pages/BookingPage";
 // axios.defaults.baseURL = 'https://placeshub.onrender.com';
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://placesserver.onrender.com';
 
 axios.defaults.withCredentials = true;
 
